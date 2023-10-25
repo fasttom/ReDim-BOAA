9915e9eab0d78724c5481d13d3da07bf77b0c156
TaeyeongKim <fasttom@hanyang.ac.kr>
2023-10-25 05:49:57 +0000

    testing the encoder loader

--- attacker/optimizer/encoder_loader.py
+++ attacker/optimizer/encoder_loader.py
@@ -1,22 +1,23 @@
 import os
 import sys
 import torch
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import torchvision
-from autoencoder.models.vgg import VGGAutoEncoder
-from autoencoder.models.resnet import ResNetAutoEncoder
+from attacker.autoencoder.models.vgg import VGGAutoEncoder
+from attacker.autoencoder.models.resnet import ResNetAutoEncoder
+from attacker.autoencoder.models.vgg import get_configs as vgg_config
+from attacker.autoencoder.models.resnet import get_configs as resnet_config
 
-
-def get_autoencoder(encoder_path:str, arch:str):
+def get_autoencoder(encoder_name:str, arch:str):
     arch = arch.lower()
     assert arch in ["vgg11", "vgg13", "vgg16", "vgg19", "resnet18", "resnet34", "resnet50", "resnet101", "resnet152"]
     if arch in ["vgg11", "vgg13", "vgg16", "vgg19"]:
-        model = VGGAutoEncoder()
+        model = VGGAutoEncoder(vgg_config(arch))
     else:
-        model = ResNetAutoEncoder()
-    true_path = (os.path.join(os.curdir,"../autoencoder/"+encoder_path))
+        model = ResNetAutoEncoder(resnet_config(arch))
+    true_path = os.path.join(os.curdir, "attacker/autoencoder/result/"+encoder_name)
     checkpoint = torch.load(true_path)
-    model.load_state_dict(checkpoint["model_state_dict"])
+    state_dict = checkpoint["state_dict"]
+    model.load_state_dict(state_dict, False)
     encoder = model.encoder
     decoder = model.decoder
     return encoder, decoder
