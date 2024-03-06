61601aedf13dd435e195b3035eba9019bc8bc793
Taeyeong Kim <fasttom@hanyang.ac.kr>
2024-03-06 04:41:02 +0000

    baysian optimization completed

---
+++ attacker/optimizer/ReDimBO.py
@@ -0,0 +1,71 @@
+import torch
+import botorch
+import numpy as np
+from botorch.models import SingleTaskGP
+from botorch.fit import fit_gpytorch_mll
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from utils import perturbate, relative_loss_gain, random_delta
+from botorch.acquisition import ExpectedImprovement, UpperConfidenceBound
+from botorch.optim import optimize_acqf
+
+def ReDimBO(image:torch.Tensor,
+            victim_model: torch.nn.Module, autoencoder: torch.nn.Module, labels: list[int], true_label:int, 
+            acquisition:str = "EI", feature_len:int = 7, num_channels:int = 512, epoch_lim: int = 200):
+    # first ten perturbations are selected randomly
+    deltas = [random_delta() for _ in range(10)]
+    perturbated_zs = [perturbate(image, delta) for delta in deltas]
+    _ = [relative_loss_gain(image, perturbated_z, labels, true_label, autoencoder, torch.nn.CrossEntropyLoss(), victim_model) for perturbated_z in perturbated_zs]
+    loss_gains, real_loss_gains = zip(*_)
+    loss_gains = list(loss_gains)
+    real_loss_gains = list(real_loss_gains)
+
+    for i in range(10): #if the attack is successful by random perturbation, 
+        # we will not use the bayesian optimization
+        if real_loss_gains[i]>0:
+            real_z = perturbated_zs[i]
+            adv_example = autoencoder.decoder(real_z)
+            return adv_example, 10, True
+    used_epoch = 10
+
+    # if given perturbations are not successful, we will use bayesian optimization
+    # until the epoch limit is reached
+    for i in range(epoch_lim-10):
+        # ititialize the model
+        gp = SingleTaskGP(deltas, loss_gains)
+        mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
+
+        # fit the model
+        fit_gpytorch_mll(mll)
+
+        # initialize the acquisition function
+        if acquisition == "EI":
+            AQ = ExpectedImprovement(gp, best_f=0)
+        elif acquisition == "UCB":
+            AQ = UpperConfidenceBound(gp, beta=0.1)
+        
+        # initialize the bounds
+        bounds = torch.stack([torch.ones(feature_len*feature_len+num_channels)*-1, torch.ones(feature_len*feature_len+num_channels)])
+
+        # optimize the acquisition function
+        candidate, acq_value = optimize_acqf(AQ, bounds=bounds, q=1, num_restarts=5, raw_samples=int(5+0.5*i))
+
+        # evaluate the candidiate
+        perturbated_z = perturbate(image, candidate)
+        loss_gain, real_loss_gain = relative_loss_gain(image, perturbated_z, labels, true_label, autoencoder, torch.nn.CrossEntropyLoss(), victim_model)
+        used_epoch += 1
+        if real_loss_gain > 0:
+            adv_example = autoencoder.decoder(perturbated_z)
+            return adv_example, used_epoch, True
+        else:
+            deltas.append(candidate)
+            perturbated_zs.append(perturbated_z)
+            loss_gains.append(loss_gain)
+            real_loss_gains.append(real_loss_gain)
+    
+    return None, np.nan, False
+
+        
+
+
+
+
--- attacker/optimizer/utils.py
+++ attacker/optimizer/utils.py
@@ -14,23 +14,25 @@ def descaler(x: torch.Tensor, param: tuple):
 def clip(x: torch.Tensor, min: float, max: float):
     return torch.max(torch.min(x, max), min)
 
-def perturbate(x: torch.Tensor, delta: torch.Tensor, feat_len:int = 7, num_channels: int = 512, min: float = -1, max: float = 1):
+def perturbate(z: torch.Tensor, delta: torch.Tensor, feat_len:int = 7, num_channels: int = 512, min: float = -1, max: float = 1):
     num_pixels = feat_len * feat_len
     delta = clip(delta, min, max)
     delta_pixel = delta[:num_pixels]
     delta_channel = delta[num_pixels:]
-    delta_pixel = delta_pixel.expand_as(x)
-    delta_channel = delta_channel.expand_as(x)
+    delta_pixel = delta_pixel.expand_as(z)
+    delta_channel = delta_channel.expand_as(z)
     abs_delta_pixel = torch.abs(delta_pixel)
     abs_delta_channel = torch.abs(delta_channel)
-    scaled_x, sclae_param = centerd_minmax_scaler(x)
-    scaled_x_perturbated = scaled_x + delta_pixel + delta_channel - abs_delta_pixel*scaled_x - abs_delta_channel*scaled_x
-    scaled_x_perturbated = clip(scaled_x_perturbated, -1, 1)
-    x_perturbated = descaler(scaled_x_perturbated, sclae_param)
-    return x_perturbated
+    scaled_z, sclae_param = centerd_minmax_scaler(z)
+    scaled_z_perturbated = scaled_z + delta_pixel + delta_channel - abs_delta_pixel*scaled_z - abs_delta_channel*scaled_z
+    scaled_z_perturbated = clip(scaled_z_perturbated, -1, 1)
+    z_perturbated = descaler(scaled_z_perturbated, sclae_param)
+    return z_perturbated
 
-def relative_loss_gain(x: torch.Tensor, perturbated_x: torch.Tensor,label_list: list[int], true_label: int, 
+def relative_loss_gain(x: torch.Tensor, perturbated_z: torch.Tensor,label_list: list[int], true_label: int, 
+                       autoencoder: torch.nn.Module,
                        loss_ft: torch.nn.Module, model: torch.nn.Module, alpha:float = 1.0):
+    perturbated_x = autoencoder.decoder(perturbated_z)
     true_label_loss = loss_ft(model(perturbated_x), torch.tensor([true_label]))
     other_label_losses = torch.min([loss_ft(model(perturbated_x), torch.tensor([label])) for label in label_list if label != true_label])
     differnece = torch.mean(torch.abs(x, perturbated_x))
@@ -38,3 +40,7 @@ def relative_loss_gain(x: torch.Tensor, perturbated_x: torch.Tensor,label_list: 
     regularized_gain = real_gain - alpha * differnece # bayesian optimize with this to minimize the differnece
     return regularized_gain, real_gain
 
+def random_delta(feat_len:int = 7, num_channels: int = 512, min: float = -1, max: float = 1):
+    num_pixels = feat_len * feat_len
+    delta = torch.rand(size=(num_pixels + num_channels)) * (max - min) + min
+    return delta
