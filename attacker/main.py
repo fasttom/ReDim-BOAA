10680d95424978c6369ca97a950fc20b27450335
Taeyeong Kim <fasttom@hanyang.ac.kr>
2024-03-15 05:13:24 +0000

    training_vgg

--- attacker/main.py
+++ attacker/main.py
@@ -11,7 +11,7 @@ epochs = 100
 train_AE = True
 
 train_loader, val_loader = load_AE_data(dataset_type="timm", dataset_name="imagenette2-320", input_size=(3, 224, 224), train_batch=256, test_batch=32)
-# attack_loader = load_vicim_data(dataset_type="timm", dataset_name="Caltech-256-Splitted", input_size=(3, 224, 224), victim_batch=32)
+attack_loader = load_vicim_data(dataset_type="timm", dataset_name="Caltech-256-Splitted", input_size=(3, 224, 224), victim_batch=32)
 
 if train_AE:
     model = train_autoencoder(train_loader, val_loader, num_layers=34, epochs=epochs)
